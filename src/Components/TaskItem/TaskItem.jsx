675e3234e53cc6cf4225ec81ab26e7ea4b66697a
Berti Camlek <berticamlek@gmail.com>
2026-07-15 11:06:47 +0200

    add TaskList

---
+++ src/Components/TaskItem/TaskItem.jsx
@@ -0,0 +1,16 @@
+import "./TaskItem.scss";
+import Button from "../Button/Button.jsx";
+
+const TaskItem = (props) => {
+    return (
+        <div className="task-item">
+            <span>{props.task}</span>
+            <span>
+                <Button classList="btn btn-secondary" title="Done"/>
+                <Button classList="btn btn-adjacent" title="Remove"/>
+             </span>
+        </div>
+    )
+}
+
+export default TaskItem;

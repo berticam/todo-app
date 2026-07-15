e60827cf916df13338ad6781ffda59761b156859
Berti Camlek <berticamlek@gmail.com>
2026-07-15 09:28:08 +0200

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'sass-reset';
+import './Main.scss'
+
+function App() {
+
+    return (
+        <>
+            <section id="center">
+                <h1>Todo</h1>
+            </section>
+        </>
+    )
+}
+
+export default App
